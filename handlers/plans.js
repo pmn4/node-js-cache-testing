91e30d69d254a319fbad0683213c8aad08e9d798
Pat Newell <pnewell+git@renttherunway.com>
2022-04-26 13:05:04 -0400

    print headers

--- handlers/plans.js
+++ handlers/plans.js
@@ -1,11 +1,18 @@
 function fnPlanDemo(req, res, _next) {
   const { headers } = req;
 
+  console.log({ headers });
+
   Object.keys(headers)
     .filter((key) => key.toLowerCase().includes("x-header-"))
-    .forEach((key) => res.set(key, headers[key]));
+    .forEach((key) => res.set(key.replace(/X-Header-/i, ""), headers[key]));
 
-  return res.send("Plans Page!");
+  return res.send(`
+  Plans Page!
+  <pre>
+  ${JSON.stringify(res.getHeaders(), null, 2)}
+  </pre>
+`);
 }
 
 module.exports = { fnPlanDemo };
